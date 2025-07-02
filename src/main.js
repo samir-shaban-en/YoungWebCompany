73410ee5f76cecde5008d1b163f38e6ca439d2c7
Samir Shaban <samirshabanpsn@gmail.com>
2025-07-02 22:50:24 +0300

    base settings

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/header';
+import './js/hero';
+import './js/books';
+import './js/feedbacks';
+import './js/events';
+import './js/footer';
+import './js/book-modal';
+import './js/contact-modal';
