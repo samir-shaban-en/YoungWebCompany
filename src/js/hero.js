8783a8ced074c54f3df920a0721c368a78176513
Yasmin <jana.afonina@gmail.com>
2025-07-05 20:25:41 +0300

    last version

--- src/js/hero.js
+++ src/js/hero.js
@@ -7,18 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
   if (heroSwiperElement) {
     const heroSwiper = new Swiper(heroSwiperElement, {
       modules: [Navigation],
-      loop: true, 
+      loop: true,
       slidesPerView: 1,
       spaceBetween: 10,
 
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-        disabledClass: 'swiper-button-disabled', 
+        disabledClass: 'swiper-button-disabled',
       },
 
       on: {
-        // Якщо потрібно буде додати якусь логіку на зміну слайда, то можна буде тут  // slideChange: function (swiper) {
+        // slideChange: function (swiper) {
         //   console.log('Slide changed to:', swiper.realIndex);
         // },
       },
