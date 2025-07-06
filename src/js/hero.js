62b70430bd446aa16e28ae40138d08f5ae44c7f1
Yasmin <jana.afonina@gmail.com>
2025-07-06 12:04:28 +0300

    last version

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,27 +1,66 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const heroSwiperElement = document.querySelector('.hero-slider');
+const swiper = new Swiper('.hero-swiper', {
+  loop: false,
+  speed: 600,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+  breakpoints: {
+    768: {
+      speed: 700,
+    },
+    1440: {
+      speed: 800,
+    },
+  },
 
-  if (heroSwiperElement) {
-    const heroSwiper = new Swiper(heroSwiperElement, {
-      modules: [Navigation],
-      loop: true,
-      slidesPerView: 1,
-      spaceBetween: 10,
+  on: {
+    init: function () {
+      updateNavButtons(this);
+    },
+    slideChange: function () {
+      updateNavButtons(this);
+    },
+  },
+});
 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        disabledClass: 'swiper-button-disabled',
-      },
+function updateNavButtons(swiperIns) {
+  const prevBut = document.querySelector('.swiper-btn-prev');
+  const nextBut = document.querySelector('.swiper-btn-next');
 
-      on: {
-        // slideChange: function (swiper) {
-        //   console.log('Slide changed to:', swiper.realIndex);
-        // },
-      },
-    });
+  if (swiperIns.isBeginning) {
+    prevBut.classList.add('button-dis');
+  } else {
+    prevBut.classList.remove('button-dis');
   }
+
+  if (swiperIns.isEnd) {
+    nextBut.classList.add('button-dis');
+  } else {
+    nextBut.classList.remove('button-dis');
+  }
+}
+
+const heroButtons = document.querySelectorAll('.hero-btn');
+
+heroButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const target = document.querySelector('.books');
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  });
 });
